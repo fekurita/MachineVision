85643a66a3068c8f6f1836a45c103f2fd5854d7e
fekurita <felipekurita@gmail.com>
2025-02-25 15:56:19 -0300

    Questao 3 feita

--- APS1/Questão3.py
+++ APS1/Questão3.py
@@ -9,11 +9,18 @@ while (frame_atual < num_frames):
 # leitura sequencia dos frames do video
     ret, frame = cap.read()
     frame_gray_new = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #transformar para uma matriz inteira de 16 bits
+    frame_gray_new_16 =frame_gray_new.astype(np.int16)
+    #Cria matriz base de comparação
     if frame_atual == 1:
-        frame_gray_old =frame_gray_new
-    frame_show =np.where(abs(frame_gray_old-frame_gray_new)<20,0,frame_gray_old)
-    cv2.imshow("Video", frame_show)
-    frame_gray_old = frame_gray_new
+        frame_base =frame_gray_new_16
+    #Calcula diferença entre frames
+    Diferenca= abs(frame_base-frame_gray_new)
+    #Cria a matriz de diferenças
+    frame_show =np.where(Diferenca<30,0,255)
+
+    cv2.imshow("Video", frame_show.astype(np.uint8))
+    #frame_gray_old = frame_gray_new_16
     frame_atual += 1
     # Press “q” on keyboard to exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
